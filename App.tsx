2cd5cfb68aca29a363e0369ab995661f603408d4
vinayagam <vinayagamkrishnan1@gmail.com>
2024-01-11 17:38:36 +0530

    web view corrections

--- App.tsx
+++ App.tsx
@@ -1,118 +1,68 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
+  Alert,
+  Button,
   SafeAreaView,
   ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import { WebView } from 'react-native-webview';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const openSSOauthentication = () => {
+    Alert.alert("asdfasdfasdf")
+  }
+
+  const handleLoad = () => {
+    console.log('Page loaded!');
   };
 
+  const handleError = (syntheticEvent: any) => {
+    const { nativeEvent } = syntheticEvent;
+    console.error(`WebView error: ${nativeEvent.description}`);
+  };
+
+  const handleMessage = (event: any) => {
+    console.log(`Received message from WebView: ${event.nativeEvent.data}`);
+  };
+
+  const injectedJavaScript = `
+    window.addEventListener('onscroll', (event) => {
+      console.log("SCROLLING>>>>>");
+    });
+  `;
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
+    <SafeAreaView>
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+      >
+        <View>
+          <Text>SAmple app</Text>
+          <Button
+            title='Button'
+            onPress={openSSOauthentication}
+          >
+          </Button>
+
+          <WebView
+            source={{ uri: 'https://github.com/vinay-org' }}
+            style={{ minHeight: 600, minWidth: 300 }}
+            injectedJavaScript={injectedJavaScript}
+            onLoad={handleLoad}
+            onError={handleError}
+            onMessage={handleMessage}
+            onNavigationStateChange={(navState) => {
+              console.log("NAVE STATE CHANGE", navState);
+            }}
+            // onScroll={() => console.log("Scrolling")}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
